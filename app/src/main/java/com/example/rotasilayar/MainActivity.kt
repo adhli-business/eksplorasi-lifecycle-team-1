c34bb263351062c4edc9b90bbfb0fd8719cbd0c0
Putra Adhli Falah <71480@fif.co.id>
2025-07-02 15:47:01 +0700

    add config changes and activity_main.xml

--- app/src/main/java/com/example/rotasilayar/MainActivity.kt
+++ app/src/main/java/com/example/rotasilayar/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.rotasilayar
 
+import android.content.res.Configuration
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -16,32 +19,56 @@ import com.example.rotasilayar.ui.theme.RotasiLayarTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            RotasiLayarTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.activity_main)
+
+        Log.d("LIFECYCLE", "Created")
+        // Ambil referensi TextView
+        val textNames = findViewById<TextView>(R.id.textNames)
+
+        // Buat daftar nama
+        val names = listOf("Adhli", "Willman", "Leon")
+
+        // Gabungkan dan tampilkan di TextView
+        textNames.text = names.joinToString(separator = "\n")
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onStart() {
+        super.onStart()
+        Log.d("LIFECYCLE", "Started")
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RotasiLayarTheme {
-        Greeting("Android")
+    override fun onResume() {
+        super.onResume()
+        Log.d("LIFECYCLE", "Resumed")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LIFECYCLE", "Paused")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LIFECYCLE", "Stopped")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LIFECYCLE", "Destroyed")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("LIFECYCLE", "Restarted")
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            Log.d("CONFIG", "Orientation changed: Landscape")
+        } else if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            Log.d("CONFIG", "Orientation changed: Portrait")
+        }
     }
 }
